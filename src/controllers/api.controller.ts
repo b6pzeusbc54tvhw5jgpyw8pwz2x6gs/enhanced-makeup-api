b38c8ca69df5914765c538b0d3fa3c9d8830ad60
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2021-05-02 19:22:43 +0900

    feat: add /brands.json, /tags.json, /categories.json, /product_types.json

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Query, Request, Route } from "@tsoa/runtime"
-import {omit} from 'lodash'
+import {omit, union, compact} from 'lodash'
 import { Request as ExRequest } from "express"
 import { APIService } from "../services/api.service"
 import { APIData } from "../types/api.type"
@@ -11,7 +11,7 @@ export class UsersController extends Controller {
    * To provide paging feature.
    */
   @Get('/v1/products.json')
-  public async proxy(
+  public async products(
     @Request() req: ExRequest,
     @Query() offset: number=0,
     @Query() limit: number=20,
@@ -23,9 +23,41 @@ export class UsersController extends Controller {
     const sliced = data.slice(offset, offset+limit)
 
     return {
-      ok: true,
+      ok: true,   
       data: sliced,
       nextOffset: offset + limit < data.length ? offset + limit : void 0
     }
+  }
+
+  @Get('/v1/brands.json')
+  public async brands(): Promise<APIData> {
+    const data = await new APIService().get('api/v1/products.json')
+    const brands = data.reduce((prev, current) => union([...prev, current.brand]), [])
+    const compacted = compact(brands)
+    return { ok: true, data: compacted }
+  }
+
+  @Get('/v1/tags.json')
+  public async tags(): Promise<APIData> {
+    const data = await new APIService().get('api/v1/products.json')
+    const tags = data.reduce((prev, current) => union([...prev, ...current.tag_list]), [])
+    const compacted = compact(tags)
+    return { ok: true, data: compacted }
+  }
+
+  @Get('/v1/categories.json')
+  public async categories(): Promise<APIData> {
+    const data = await new APIService().get('api/v1/products.json')
+    const categories = data.reduce((prev, current) => union([...prev, current.category]), [])
+    const compacted = compact(categories)
+    return { ok: true, data: compacted }
+  }
+
+  @Get('/v1/product_types.json')
+  public async productTypes(): Promise<APIData> {
+    const data = await new APIService().get('api/v1/products.json')
+    const productTypes = data.reduce((prev, current) => union([...prev, current.product_type]), [])
+    const compacted = compact(productTypes)
+    return { ok: true, data: compacted }
   }
 }
